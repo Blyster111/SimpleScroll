e23a6a239dd7088aecae4bb9fed08f20bb590bb9
Blyster111 <blyster111@gmail.com>
2021-08-31 12:10:23 +0200

    Update SimpleScrollItemObject.cs

--- SimpleScrollItemObject.cs
+++ SimpleScrollItemObject.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 namespace Robin
 {
     public abstract class SimpleScrollItemObject : MonoBehaviour
-    {
+    {   
+        /// <summary>
+        /// Index of the allItems[].
+        /// </summary>
         [HideInInspector]
         public int Index;
+        /// <summary>
+        /// My Item Data. (Item of allItems[])
+        /// </summary>
         [HideInInspector]
         public SimpleScrollItem MyItem;
+        /// <summary>
+        /// The SimpleScroll instance.
+        /// </summary>
         [HideInInspector]
         public SimpleScroll MyScroll;
         public abstract void OnItemRedraw();
-
-        public void MoveToIndex(int newIndex)
-        {
-            MyScroll.RepositionItem(Index, newIndex);
-        }
     }
 }
